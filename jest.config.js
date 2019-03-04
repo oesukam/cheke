0e60a79a1e63aad0cf11f8d5e5091f149c9ab830
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-03-04 21:24:56 +0200

    First Commit

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  testEnvironment: 'node',
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  // coverageReporters: [
+  //   "json",
+  //   "text",
+  //   "lcov",
+  //   "clover"
+  // ],
+
+  // An array of file extensions your modules use
+  // moduleFileExtensions: [
+  //   "js",
+  //   "json",
+  //   "jsx",
+  //   "node"
+  // ],
+
+  // The glob patterns Jest uses to detect test files
+  // testMatch: [
+  //   "**/__tests__/**/*.js?(x)",
+  //   "**/?(*.)+(spec|test).js?(x)"
+  // ],
+
+  // Indicates whether each individual test should be reported during the run
+  // verbose: null,
+};
