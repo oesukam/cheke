d631c3200d74b56539e4eb85343a3588b8e39a4b
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-03-05 22:07:58 +0200

    feat: fix houndci violations

--- __tests__/hasErrors.test.js
+++ __tests__/hasErrors.test.js
@@ -1,30 +1,26 @@
 const hasErrors = require("../src/hasErrors");
 
 describe("hasErrors", () => {
-  test("should return false if you don't provide reqRules", () => {
-    return hasErrors().then(res => {
+  test("should return false if you don't provide reqRules", () =>
+    hasErrors().then(res => {
       expect(res).toBeFalsy();
-    });
-  });
+    }));
 
-  test("should return name's rule can not be empty", () => {
-    return hasErrors({ reqRules: { name: "" } }).catch(err => {
+  test("should return name's rule can not be empty", () =>
+    hasErrors({ reqRules: { name: "" } }).catch(err => {
       expect(err.message).toBe("name's rule can not be empty");
-    });
-  });
+    }));
 
-  test("should return The age must be a number", () => {
-    return hasErrors({
+  test("should return The age must be a number", () =>
+    hasErrors({
       data: { age: "21" },
       reqRules: { age: "number" }
     }).then(res => {
       expect(res.age).toBe("The age must be a number.");
-    });
-  });
+    }));
 
-  test("should return The name field is required.", () => {
-    return hasErrors({ reqRules: { name: "required" } }).then(res => {
+  test("should return The name field is required.", () =>
+    hasErrors({ reqRules: { name: "required" } }).then(res => {
       expect(res.name).toBe("The name field is required.");
-    });
-  });
+    }));
 });
