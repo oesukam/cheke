f5c4005aaf41fbc144d0ae16e50b89e8e9f1ce03
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-03-07 13:19:41 +0200

    feat: add email's validation
    - write unit tests
    - implement email's validation
    
    [Delivers #164459413]

---
+++ src/messages/notEmailMessage.js
@@ -0,0 +1,1 @@
+module.exports = label => `The ${label} must be a valid email address.`;
---
+++ src/validators/email.js
@@ -0,0 +1,8 @@
+const notEmailMessage = require('../messages/notEmailMessage');
+
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line no-useless-escape
+
+module.exports = ({ value, label } = {}) => {
+  if (EMAIL_REGEX.test(value)) return false;
+  return notEmailMessage(label);
+};
