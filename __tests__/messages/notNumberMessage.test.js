d34aa20bc73f2931f7b8b848475532aaf20895d3
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-03-07 19:03:52 +0200

    feat: add integer's validation unit tests

--- __tests__/messages/notNumberMessage.test.js
+++ __tests__/messages/notNumberMessage.test.js
@@ -1,7 +1,7 @@
 const notNumber = require('../../src/messages/notNumberMessage');
 
 describe('not-a-number-message', () => {
-  test('should return The username must be a number.', () => {
-    expect(notNumber('username')).toBe('The username must be a number.');
+  test('should return The age must be a number.', () => {
+    expect(notNumber('age')).toBe('The age must be a number.');
   });
 });
