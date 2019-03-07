a4781d46b3369f2e890c98825e2a10aa34064d99
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-03-07 17:21:25 +0200

    feat: refactor number's validation to avoid code duplication

--- src/validators/number.js
+++ src/validators/number.js
@@ -1,6 +1,4 @@
 const numberMessage = require('../messages/notNumberMessage');
 
-module.exports = ({ value, label } = {}) => {
-  if (typeof value === 'number') return false;
-  return numberMessage(label);
-};
+module.exports = ({ value, label } = {}) =>
+  typeof value === 'number' ? false : numberMessage(label);
