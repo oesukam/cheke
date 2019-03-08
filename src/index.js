baf711a8f2afa9707fbf9b27b61a0fa26048862e
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-03-08 09:19:05 +0200

    chore: fix houndci violations

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 const hasErrors = require('./hasErrors');
 
 const requests = ['params', 'body', 'query'];
-const cheke = (args = { errors: 'errors' }) => async (req, res, next) => {
-  if (typeof args.errors !== 'string') throw Error('errors should be a string');
+const cheke = ({ errors = 'errors', ...args }) => async (req, res, next) => {
+  if (typeof errors !== 'string') throw Error('errors should be a string');
   let failed;
-  for (const value of requests) {
-    if (args[value]) {
-      failed = await hasErrors({
-        data: req[value],
-        reqRules: args[value],
-        path: value,
-      });
-    }
-  }
+  await new Promise(resolve => {
+    requests.forEach(async value => {
+      if (failed) return;
+      if (args[value]) {
+        failed = await hasErrors({
+          data: req[value],
+          reqRules: args[value],
+          path: value,
+        });
+        resolve();
+      }
+    });
+  });
+
   if (failed) {
-    return res.status(400).json({ [args.arrors]: failed });
+    return res.status(400).json({ [errors]: failed });
   }
   return next();
 };
