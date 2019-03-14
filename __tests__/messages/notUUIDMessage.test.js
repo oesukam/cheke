e3b7517490c9fe823ebbe09e19a39b63aaeb2188
Olivier ESUKA MUSELEMU <oesukam@gmail.com>
2019-03-14 15:24:35 +0200

    feat: corrected test describe messages

--- __tests__/messages/notUUIDMessage.test.js
+++ __tests__/messages/notUUIDMessage.test.js
@@ -1,6 +1,6 @@
 const notUUID = require('../../src/messages/notUUIDMessage');
 
-describe('not-a-date-message', () => {
+describe('not-a-uuid-message', () => {
   test('should return The userId is not a valid UUID.', () => {
     expect(notUUID('userId')).toBe('The userId is not a valid UUID.');
   });
